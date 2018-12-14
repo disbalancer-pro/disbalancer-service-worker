9624facc502099435a5454c5e29e1b1892d13c1c
Marcelo McAndrew <marcelomcandrew@gmail.com>
2018-12-14 15:05:12 -0500

    added support for multiple assests

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ origin.listen(5000, function() {
 
 // Server to replicate a content node
 const edge = express();
+
+edge.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 edge.use(express.static('edge'));
 edge.listen(5001, function() {
     console.log("Edge server listening on 5001");
--- origin/service-worker.js
+++ origin/service-worker.js
@@ -1,24 +1,42 @@
-
+// install stage
 self.addEventListener('install', function(event) {
+  // we dont need to wait for other clients to close
   self.skipWaiting();
 });
 
+// active stage
 self.addEventListener('activate', function(event) {
+  // wait until
   event.waitUntil(function(event) {
+    // we can take control of this existing session w/o reloading it
     return self.clients.claim();
   });
 });
 
-self.onfetch = function(event) {
-  if (event.request.url.includes('dog.jpg')) {
+self.addEventListener('fetch', function(event) {
+  console.log("requested resource: " + event.request.url)
+  var url = event.request.url
+  var route = url.split("/")[3]
+
+  if (route != "" && !route.includes(".js")) {
+    console.log("route: " + route)
     var init = {
       method: 'GET',
       mode: event.request.mode,
       cache: 'default'
     };
-    var url = 'http://localhost:5001/cat.jpg';
-    event.respondWith(fetch(url, init));
-  } else {
-    event.respondWith(fetch(event.request));
+
+    url = "http://localhost:5001/" + route
+    console.log("fetching: " + url)
+    event.respondWith(fetch(url, init).then(
+      function(res) {
+        console.log("RES: ", res);
+        return res
+      },
+      function(err) {
+        console.log("ERROR: ",err);
+        return err
+      })
+    )
   }
-};
+})
