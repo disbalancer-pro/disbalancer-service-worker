89d6e39a87e95c7cd61e97eb130d69047e2f40d5
Marcelo McAndrew <marcelomcandrew@gmail.com>
2019-02-20 13:43:08 -0500

    SW can serve the blog over https

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,49 +3,40 @@
 
 const CACHE = 'gladius-cache-v1'
 const WEBSITE = 'blog.gladius.io'
-const HOST = 'localhost'
+const HOST = 'blog.gladius.io'
 const SEEDNODE = 'http://blog.gladius.io:8080'
 const MASTERNODE = 'https://blog.gladius.io'
 const MNLIST = 'https://blog.gladius.io/docile-stu'
-const EDGENODE = 'http://X.X.X.X:8080'
 const MNONLINE = false
 
 let mnResponse = {
   "assetHashes": {
-    "/1.9c302b2a2acf7a91fb09.css": "5cb33cc1d7fe335475313ed547333dad18a0042e6022eedc088cf4c6c587663f",
-    "/static/d/921/path---development-update-17-08-17-be-0-110-8tJcpJsbJvQdOWjqQc1OklZLClg.json": "19ea6e5595a945351c2c1df5eae5351efcfd1cb616f7363d4653f84ab03702c6",
-    "/workbox-v3.6.3/workbox-strategies.prod.js": "96b654f16c850c2c437a47268a6f6741045baadf30ab8f5b9815cbc2241c0b69",
-    "/offline-plugin-app-shell-fallback/index.html": "c30aa645b3f1d903c3e00db659c182463fc5f4b7064d7a59d7e5fc3ba080e8f6",
-    "/static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json": "2aba5a0cedbcb2c9688ff6ad7bd23d3c9a9eda4e3c35b65c7eb80b9216f45ad2",
-    "/component---src-templates-blog-post-jsx-930512844aaaab306d69.js": "b4607fef5acef37406309b1558b1bd7ecdbe397b8c2ac9f85429dd7e0584b8bf",
-    "/sw.js": "5e1fafb93efa00fd1b8d299f134e96f04c6d94ae606c650bc43acd3f51e6c262",
-    "/workbox-v3.6.3/workbox-core.prod.js": "8a4692e0ce9d06bab525501aa85639bb10b8395c0ac46c76a5dc1c589e826486",
-    "/0-458e90dba83ef73bda5e.js": "7b92125706ba4947ca8359922127ab897ba258cfbe714f17d6f8ffb31ab590a5",
-    "/component---src-pages-404-jsx-2e922e42565e0e43876b.js": "68c0fa7f1cb2638bbf131edd5e50eb8880f84c2c15cd59d1893f525ed771109f",
-    "/workbox-v3.6.3/workbox-sw.js": "eb06946054b05d7ebe88169caf2b11f711c605edcfca6cf4ceca0aaa0c36c967",
-    "/component---src-pages-index-jsx-606e4b2263e6265f3cbb.js": "2c7625a0d24c3a0e405f9d9e1aa6dec65badb26dac16e1d60810fbf38116f6ae",
-    "/static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json": "2aba5a0cedbcb2c9688ff6ad7bd23d3c9a9eda4e3c35b65c7eb80b9216f45ad2",
-    "/static/d/28/path---gladius-node-system-requirements-6-af-afb-E5TY9Rj4gBpp3KUtwOgQ0gIPNfo.json": "ac3755cccc3ade542356c23c723ad5bb8dd719c14977ddf0688909e372d0c067",
-    "/workbox-v3.6.3/workbox-routing.prod.js": "a696a1298270af6e5c547cd0fd5df4b455ec486d9f832ad9d1b07e9f632ccb88",
-    "/site.webmanifest": "310b869434f0ee9d99a110e5ef6bfb41eac115bce2428f562ff5df14378519ff",
-    "/workbox-v3.6.3/workbox-precaching.prod.js": "0068f50cd7685dd1191a494ae657377f21797fc573ac5f9b1e7c9bafe8fa4d8d",
-    "/static/d/286/path---gladius-open-beta-launch-rewards-program-ffa-3d3-fRjsqEM2lUFNHpMi5b7Ov0TqfY.json": "c6728870a7b1a4e97dc80a6517a6f046166d45ddedd8afac334aabfd50b7ea8e",
-    "/static/d/700/path---gladius-partners-with-digital-ocean-9-be-9bd-8MH8XgzI06xEpLEkvfHtxHH6VQ.json": "5549a2b16373ecfd8cd90fbc1afc75ad5ebf65883f1feaab7186d53792fb1d61",
-    "/": "472e9510a94faa1ce08e044f8498c07121c1a61de1fe88d990163d47d2772513",
-    "/1-cfc2fe5656b849c3bc22.js": "e3b1695acdcba900d4dda22a93c42f5dc72a3190ca8843c71ac90f4c09e45c61",
-    "/webpack-runtime-e3ba47d3ca04d4c11147.js": "e03bc514184800bf31699a8ba30b6f7be564c88d0f22066d3152d7e7eaa8ef3a",
-    "/app-3a9fab6814e6cb51feae.js": "adb1363be87bce937fbd290332168436a8081a208e49b8fc3ca238fdcd62107a",
-    "/static/d/672/path---index-6a9-Exrs9ddYxm2BNZvR7PjAyW54HhY.json": "91137914d91f9eb0f0271e6cc023bf436cd550653918cc2a34975740699bf4fc",
-    "/8-d36bf60b4184ec6b38dc.js": "c80ce99d7a5c7cf8102e6749e73dfe91e552388cd9d88aafa90327a1fcda78f4",
-    "/component---node-modules-gatsby-plugin-offline-app-shell-js-8d468fe74d4d22168837.js": "6f038a3e3a8d9154df235dfff849b263f613a256906b3c46fcee5183542668f1",
-    "/static/d/223/path---participating-in-gladius-public-pre-sale-c-5-c-fde-BdqjxDRCW3tp8Ac9a5PEnz6rA.json": "26242216d605d2003a3dbbbc0ac17982112a386c7f5eeb35f57349bc458ee195",
+    "/component---src-templates-blog-post-jsx-c65019d94aacb4255eb4.js": "eae35559dae5a6c3045823b422b5e5584c2302d933d6d2e3a260e9743e357e4a",
+    "/0-9452b028c0c63d766294.js": "6ba2d9faf76eb13f81c8b2af7359ed209e064bd522b73fb319e1f63e6ff3f461",
+    "/api/v1/pods": "cbf1fdfdb7257daf8b0905d94bd04e2829c502c9c01b1d96bb979069e2ebc895",
+    "/static/d/978/path---gladius-node-system-requirements-6-af-afb-F7VFRyTXrKoeIEWPC2xRXb5eKRE.json": "0021d411925ee700ddf66a58df9ff116cf2e54ce9e2284b440dc47e2f5ebe637",
+    "/": "114eb75ed4a010ffe764063c9b1e62a7bdcf4d36d899a28d7ec1fdb0927a72d1",
+    "/pages-manifest-a59a9b14babd21544c52.js": "f0f763984de40d22b02eb62dc51c457b2da40e993805848fda0b9b90c2f46d0e",
+    "/imp/test.php": "887c8ada6058f01125a5131f1c495ba5f0171b2c40466ea824494403b87c1a22",
+    "/1-35a3119f4bd4d69c5356.js": "473f8aac07094d08eb3240cf669411dc98f5607d6e186cd337c81052eabe63e2",
+    "/favicon.ico": "eea4f9f9f33015be7589f3bddb4061a5d4d44dc731b6593d2c1cf5030d7fd5cd",
+    "/.DS_Store": "887c8ada6058f01125a5131f1c495ba5f0171b2c40466ea824494403b87c1a22",
+    "/developer": "cbf1fdfdb7257daf8b0905d94bd04e2829c502c9c01b1d96bb979069e2ebc895",
+    "/static/d/663/path---gladius-partners-with-digital-ocean-9-be-9bd-rlHy22hzDzTlKw24wAdqMXH4.json": "0e9ba8a33f14ac13c33f56ce95b1ea31e39fd2bc9fe4e1682dd2941dff137afc",
+    "/component---src-pages-index-jsx-2918ee385d89424c0187.js": "b5920228d900e2cd1b2e6a7f96b5d0be4b2aaeecd93072dc97be820cc1742508",
+    "/app-2d61a7a6a4bf01441be5.js": "dd15140ba52f86cf591747f0b54e3a734e10454677c60f7b21288b8dc758fc0f",
+    "/gladius-node-system-requirements/": "58223d7c53c415e3f539fdf57296c941255a8f420297dae76b7db68cf888f6b8",
+    "/static/d/632/path---index-6a9-PB7wvjHk4PfGcA6zbEVWoh6BtZI.json": "af2fb12ae0bc18662955b21ecc86e89c4df43e9bdd336ad845b55e4036a3d5d3",
+    "/webpack-runtime-8dd6218e3a37d039210b.js": "6164e6839f8afe1bfad9c1240387abf09a7303e6325bfb310eda9f796732dec8",
+    "/robots.txt": "887c8ada6058f01125a5131f1c495ba5f0171b2c40466ea824494403b87c1a22",
+    "/static/d/673/path---development-update-17-08-17-be-0-110-Mq1xK98NvJj7wEYcx1yfPSJthM.json": "29af607eca7885d59fd15b5ea5e93a5ef23c8e0269a33e78ce0e5093b452ea35",
+    "/static/d/240/path---gladius-open-beta-launch-rewards-program-ffa-3d3-rQtDlrghi1aHCbpbCW4PSy6OFc.json": "a47f21c7a164540eff77b32d50e4594f0231c65100112c128037a9d748fc009e"
   },
-  "edgeNodes": []
+  "edgeNodes": ["https://0xe8ebd13306f6f27562cf471ddde325985a1f76fe.cdn.beta.gladiuspool.com:8080"]
 }
 
 // install stage
 self.addEventListener('install', function(event) {
-  // event.waitUntil(retrieveList(MNLIST));
   self.skipWaiting();
 });
 
@@ -62,10 +53,7 @@ self.addEventListener('fetch', function(event) {
   // serve from cache, fallback on network
   event.respondWith(fromCacheThenNetwork(event.request));
   // fetch requests again in order to update the cache
-  event.waitUntil(update(EDGENODE, event.request));
-  // event.waitUntil(retrieveList(MNLIST));
-
-  // event.respondWith(fetch(event.request.url))
+  event.waitUntil(update("https://0xe8ebd13306f6f27562cf471ddde325985a1f76fe.cdn.beta.gladiuspool.com:8080", event.request));
 });
 
 // open a cache and use `addAll()` with an array of assets to add all of them
@@ -110,20 +98,19 @@ function checkHash(expectedHash, response) {
 // serve the content from cache if it's there, if not fallback to the network
 function fromCacheThenNetwork(request) {
   let url = new URL(request.url);
-  return findHash(url.pathname).then(function(hash) {
-
-    console.log("HASH",hash);
+  return findHash(url.pathname).then(function(hash) { // 1) FIND HASH IN LIST
 
     let asset = {
       "name" : url.pathname,
       "hash" : hash
     }
     // first find in local cache then try the edge nodes then the masternode
-    return caches.match(request).then(function(cachedContent) {
+    return caches.match(request).then(function(cachedContent) { // 2) SEE IF RESPONSE IS ALREADY IN CACHE
+      // if content is cached
       if (cachedContent) {
         console.log("serving " + asset.name + " from cache");
       } else {
-        // if its not for our site or edgenode doesnt have it, proxy it to the masternode
+        // if its not for our site (different host) or edgenode doesnt have it (no asset hash), proxy it to the masternode
         if (!url.hostname.includes(HOST) || !asset.hash) {
           console.log("proxied " + url.pathname + " from " + url.origin);
           return fetch(request.url).then(function(response) {
@@ -133,31 +120,41 @@ function fromCacheThenNetwork(request) {
         }
       }
 
-      // if we get here, we assume the content is either cached or on an edgenode
-      // build the url
-      let edgeUrl = EDGENODE + "/content?website=" + WEBSITE + "&asset=" + asset.hash
-      let eurl = new URL(edgeUrl)
+      return pickEdgeNode(mnResponse).then(function(edgenode){ // 3) PICK AN EDGENODE FROM THE LIST
+        // if we get here, we assume the content is either cached or on an edgenode
+        // build the url
+        console.log("Edgenode found",edgenode);
+        let edgeUrl = edgenode + "/content?website=" + WEBSITE + "&asset=" + asset.hash
+        let eurl = new URL(edgeUrl)
 
-      // return from cache or fetch from network (edge node)
-      return cachedContent || fetch(edgeUrl,{method:"GET",headers:{"Accept-Encoding": "gzip, deflate"}}).then(function(response) {
-        // check the hash first to make sure its the same file
-        return checkHash(asset.hash,response.clone()).then(function(res) {
-          // rebuild the response in order to set the correct headers
-          console.log("serving " + asset.name + " from " + eurl.origin);
-          return rebuildResponse(res.clone(), asset.name).then(function(response) {
-            // add it to the cache for future use
-            addToCache(request.url, response.clone())
-            return response
-          })
+        // return from cache or fetch from network (edge node)
+        return cachedContent || fetch(edgeUrl).then(function(response) { // 4) FETCH CONTENT FROM CACHE OR EDGENODE
+          // check the hash first to make sure its the same file
+          return checkHash(asset.hash,response.clone()).then(function(res) { // 5) CHECK THE CONTENT HASH
+            console.log("serving " + asset.name + " from " + eurl.origin);
+            // rebuild the response in order to set the correct headers
+            return rebuildResponse(res.clone(), asset.name).then(function(response) { // 6) REBUILD AND RETURN THE RESPONSE
+              // return and add it to the cache for future use
+              addToCache(request.url, response.clone())
+              return response
+            })
+          }).catch(function(err){
+            // if the asset fails the hash check
+            Error(err)
+            console.log("Asset failed the hash check! Using default fallback.");
+            return useFallback()
+          });
+          // TODO: im pretty sure i can remove this and use the parent catch...
         }).catch(function(err){
-          // if the asset fails the hash check
-          console.log(err);
-          console.log("Asset failed the hash check! Using default fallback.");
-          return useFallback()
-        });
-      }).catch(function(err){
-        // if the fetch to the edgenode was unsuccessful (return from masternode)
-        console.log(err);
+          // if the fetch to the edgenode was unsuccessful (prxoy to masternode)
+          Error(err)
+          console.log("Cache || Fetch err");
+          return fetch(request.url)
+        })
+      }).catch(function(){
+        // if the fetch to the edgenode was unsuccessful (proxy to from masternode)
+        Error(err)
+        console.log("pick edgenode");
         return fetch(request.url)
       })
     }).catch(function(err) {
@@ -258,6 +255,37 @@ function findHash(assetName) {
     return retrieveList(MNLIST).then(function(res){
       return res.json().then(function(hashList) {
         return hashList.assetHashes[assetName]
+      })
+    })
+  })
+}
+
+// takes the MN json response and picks an egdenode from the list
+function pickEdgeNode(mnList) {
+  // makes things work when the masternode is down
+  if (!MNONLINE) {
+    return new Promise(function(resolve,reject){
+      let edgenode = mnList.edgeNodes[0]
+      console.log(edgenode,"found in fake api");
+      resolve(edgenode)
+    })
+  }
+  // the list should be in the cache but if it's not, go get it and cache it
+  return caches.match(mnList).then(function(mnResponse){
+    console.log("then");
+    console.log(mnResponse);
+    return mnResponse.json().then(function(res){
+      // just pick the first node for now...
+      return res.edgeNodes[0]
+    })
+  }).catch(function(err){
+    console.log(err);
+    return retrieveList(mnList).then(function(mnResponse){
+      console.log("catch:");
+      console.log(mnResponse);
+      return mnResponse.json().then(function(res){
+        // just pick the first node for now...
+        return res.edgeNodes[0]
       })
     })
   })
