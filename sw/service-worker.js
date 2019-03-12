0c8424db983e508dde3f88ea945572a3b0954767
Marcelo <10275440+kleoken@users.noreply.github.com>
2019-03-12 10:50:03 -0400

    Revert "Feature/docker"

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -1,6 +1,39 @@
 // service worker cache polyfill
 // importScripts('serviceworker-cache-polyfill.js')
 
+const CACHE = 'gladius-cache-v1'
+const WEBSITE = 'blog.gladius.io'
+const MASTERNODE = 'https://blog.gladius.io'
+const MNLIST = 'https://blog.gladius.io/docile-stu' // masternode list endpoint
+const MNONLINE = false // if you want to use the "local" response, turn this to false
+
+// this is for testing purposes, it simulates the response from the masternode
+const MNRESPONSE = {
+  "assetHashes": {
+    "/component---src-templates-blog-post-jsx-c65019d94aacb4255eb4.js": "eae35559dae5a6c3045823b422b5e5584c2302d933d6d2e3a260e9743e357e4a",
+    "/0-9452b028c0c63d766294.js": "6ba2d9faf76eb13f81c8b2af7359ed209e064bd522b73fb319e1f63e6ff3f461",
+    "/api/v1/pods": "cbf1fdfdb7257daf8b0905d94bd04e2829c502c9c01b1d96bb979069e2ebc895",
+    "/static/d/978/path---gladius-node-system-requirements-6-af-afb-F7VFRyTXrKoeIEWPC2xRXb5eKRE.json": "0021d411925ee700ddf66a58df9ff116cf2e54ce9e2284b440dc47e2f5ebe637",
+    "/": "114eb75ed4a010ffe764063c9b1e62a7bdcf4d36d899a28d7ec1fdb0927a72d1",
+    "/pages-manifest-a59a9b14babd21544c52.js": "f0f763984de40d22b02eb62dc51c457b2da40e993805848fda0b9b90c2f46d0e",
+    "/imp/test.php": "887c8ada6058f01125a5131f1c495ba5f0171b2c40466ea824494403b87c1a22",
+    "/1-35a3119f4bd4d69c5356.js": "473f8aac07094d08eb3240cf669411dc98f5607d6e186cd337c81052eabe63e2",
+    "/favicon.ico": "eea4f9f9f33015be7589f3bddb4061a5d4d44dc731b6593d2c1cf5030d7fd5cd",
+    "/.DS_Store": "887c8ada6058f01125a5131f1c495ba5f0171b2c40466ea824494403b87c1a22",
+    "/developer": "cbf1fdfdb7257daf8b0905d94bd04e2829c502c9c01b1d96bb979069e2ebc895",
+    "/static/d/663/path---gladius-partners-with-digital-ocean-9-be-9bd-rlHy22hzDzTlKw24wAdqMXH4.json": "0e9ba8a33f14ac13c33f56ce95b1ea31e39fd2bc9fe4e1682dd2941dff137afc",
+    "/component---src-pages-index-jsx-2918ee385d89424c0187.js": "b5920228d900e2cd1b2e6a7f96b5d0be4b2aaeecd93072dc97be820cc1742508",
+    "/app-2d61a7a6a4bf01441be5.js": "dd15140ba52f86cf591747f0b54e3a734e10454677c60f7b21288b8dc758fc0f",
+    "/gladius-node-system-requirements/": "58223d7c53c415e3f539fdf57296c941255a8f420297dae76b7db68cf888f6b8",
+    "/static/d/632/path---index-6a9-PB7wvjHk4PfGcA6zbEVWoh6BtZI.json": "af2fb12ae0bc18662955b21ecc86e89c4df43e9bdd336ad845b55e4036a3d5d3",
+    "/webpack-runtime-8dd6218e3a37d039210b.js": "6164e6839f8afe1bfad9c1240387abf09a7303e6325bfb310eda9f796732dec8",
+    "/robots.txt": "887c8ada6058f01125a5131f1c495ba5f0171b2c40466ea824494403b87c1a22",
+    "/static/d/673/path---development-update-17-08-17-be-0-110-Mq1xK98NvJj7wEYcx1yfPSJthM.json": "29af607eca7885d59fd15b5ea5e93a5ef23c8e0269a33e78ce0e5093b452ea35",
+    "/static/d/240/path---gladius-open-beta-launch-rewards-program-ffa-3d3-rQtDlrghi1aHCbpbCW4PSy6OFc.json": "a47f21c7a164540eff77b32d50e4594f0231c65100112c128037a9d748fc009e"
+  },
+  "edgeNodes": ["https://0xe8ebd13306f6f27562cf471ddde325985a1f76fe.cdn.beta.gladiuspool.com:8080"]
+}
+
 // install stage
 self.addEventListener('install', function(event) {
   console.log("SW installing...");
@@ -25,18 +58,19 @@ self.addEventListener('activate', function(event) {
 // every time a resource is requested
 self.addEventListener('fetch', function(event) {
   // serve from cache, fallback on network
-  if(isResource(event.request)) {
-    event.respondWith(fromCacheThenNetwork(event.request));
-    // fetch requests again in order to update the cache
-    event.waitUntil(updateCache(event.request));
-  } else {
-    event.respondWith(fetch(event.request));
-  }
+  event.respondWith(fromCacheThenNetwork(event.request));
+  // fetch requests again in order to update the cache
+  event.waitUntil(updateCache(event.request));
 });
 
-// A request is a resource request if it is a `GET`
-function isResource(request) {
-  return request.method === 'GET';
+// open a cache and use `addAll()` with an array of assets to add all of them
+// to the cache. Return a promise resolving when all the assets are added.
+function preCache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      'mstile-150x150.png'
+    ]);
+  });
 }
 
 // add a request/response pair to the cache
@@ -58,7 +92,7 @@ function removeFromCache(request) {
 }
 
 // check hash of response against expected hash (name of file from masternode)
-async function assertHash(expectedHash, response) {
+async function checkHash(expectedHash, response) {
   const clone = await response.clone()
   const content = await clone.blob()
   const fr = new FileReader()
@@ -112,7 +146,7 @@ async function fromCacheThenNetwork(request) {
     const edgeResponse = await fetch(edgeUrl)
 
     // 5. Check the hash
-    const checkedResponse = await assertHash(asset.hash, edgeResponse.clone())
+    const checkedResponse = await checkHash(asset.hash, edgeResponse.clone())
 
     // 6. Rebuild the response with the correct headers
     const rebuiltResponse = await rebuildResponse(checkedResponse.clone(), asset.name)
@@ -152,7 +186,7 @@ async function updateCache(request, mnList) {
   let check
   try{
     // 3. Do a hash comparison
-    check = await assertHash(hash,cachedContent.clone())
+    check = await checkHash(hash,cachedContent.clone())
   } catch (err) {
     console.warn("UC: wrong hash, proxying to edgenode");
     // if it's the wrong hash go get new content
@@ -167,7 +201,7 @@ async function updateCache(request, mnList) {
       const edgeResponse = await fetch(edgeUrl)
 
       // 5. Check the hash
-      const checkedResponse = await assertHash(hash,edgeResponse.clone())
+      const checkedResponse = await checkHash(hash,edgeResponse.clone())
 
       // 6. Rebuild the response with the correct headers
       const rebuiltResponse = await rebuildResponse(checkedResponse.clone(), url.pathname)
@@ -234,6 +268,10 @@ async function rebuildResponse(response, assetName) {
 
 // match the name to the hash
 async function findHash(mnList, assetName) {
+  if (!MNONLINE) {
+    return findLocalHash(assetName)
+  }
+
   // find mnlist response in cache
   const cache = await caches.match(mnList)
   if (cache) {
@@ -268,6 +306,10 @@ async function findHash(mnList, assetName) {
 
 // takes the MN json response and picks an egdenode from the list
 async function pickEdgeNode(mnList) {
+  if (!MNONLINE) {
+    return pickLocalEdgeNode()
+  }
+
   // find mnlist response in cache
   const cache = await caches.match(mnList)
   if (cache) {
@@ -300,6 +342,31 @@ async function pickEdgeNode(mnList) {
   return node
 }
 
+// match the name to the hash
+// uses the local "fake" api response
+async function findLocalHash(assetName) {
+  // makes things work when the masternode is down
+  const asset = MNRESPONSE.assetHashes[assetName]
+  if (asset === undefined) {
+    throw new Error("FLH: " + assetName + " NOT found in the fake api")
+  }
+    console.log("FLH: ",assetName, "found in the fake api");
+    return asset
+}
+
+// pick an edgenode for lists
+// uses the local "fake" api response
+async function pickLocalEdgeNode() {
+  // makes things work when the masternode is down
+  const edgenode = MNRESPONSE.edgeNodes[0]
+  if (edgenode === undefined) {
+    console.warn("PLEN: edgenode not found in the fake api");
+    throw new Error("PLEN:",err)
+  }
+    console.log("PLEN:",edgenode, "found in the fake api");
+    return edgenode
+}
+
 // array buffer to hex
 function bufferToHex(buffer) {
     var s = '', h = '0123456789ABCDEF';
@@ -326,7 +393,6 @@ function retrieveList(url) {
   })
 }
 
-// proxy to masternode
 async function proxyToMasterNode(request) {
   const url = new URL(request.url);
   console.log("PTMN: proxied " + url.pathname + " to masternode");
@@ -336,3 +402,19 @@ async function proxyToMasterNode(request) {
   }
   return res
 }
+
+// This fallback never fails since it uses embedded fallbacks.
+function useFallback() {
+  return Promise.resolve(new Response(FALLBACK, { headers: {
+    'Content-Type': 'image/svg+xml'
+  }}));
+}
+
+// The fallback is an embedded SVG image.
+const FALLBACK =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="180" stroke-linejoin="round">' +
+  '  <path stroke="#DDD" stroke-width="25" d="M99,18 15,162H183z"/>' +
+  '  <path stroke-width="17" fill="#FFF" d="M99,18 15,162H183z" stroke="#eee"/>' +
+  '  <path d="M91,70a9,9 0 0,1 18,0l-5,50a4,4 0 0,1-8,0z" fill="#aaa"/>' +
+  '  <circle cy="138" r="9" cx="100" fill="#aaa"/>' +
+  '</svg>';
