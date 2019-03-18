8a00bb34df55f0d7f58ba90f734f02f2cdb01a08
Nathan Grey <nate@studioember.com>
2019-03-18 15:24:39 -0400

    Removes the source file

--- docker.sh
+++ docker.sh
@@ -5,6 +5,9 @@ echo "const CACHE = '$SW_CACHE$(date +%s)';const WEBSITE = '$SW_DOMAIN';const MA
 
 cat /data/service-worker.js >> /build/service-worker.js
 
+# Remove source file
+rm -rf /data/service-worker.js
+
 # Obfuscator
 # https://github.com/javascript-obfuscator/javascript-obfuscator/
 javascript-obfuscator /build/service-worker.js --domainLock ["$SW_DOMAIN"] --disableConsoleOutput true --output /build/service-worker.js --compact true --self-defending true
